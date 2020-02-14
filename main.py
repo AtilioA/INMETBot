123c3a88b2a3883ed2307754d18ae94c41896ec3
Atílio Antônio Dadalto <atiliodadalto@hotmail.com>
2020-02-13 23:20:13 -0300

    Add alerts parser and action decorators (#1)
    * Update webserver
    
    * Add alerts parser and action decorators
    
    Actions decorators allow signaling different actions while handling a command

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import telegram
 from telegram.ext import Updater, CommandHandler
 from configparser import ConfigParser
 from bs4 import BeautifulSoup
@@ -5,6 +6,8 @@ import requests
 import os
 import web
 import scrap_satelites
+import parse_alerts
+from functools import wraps
 
 # To prevent the bot from sleeping
 urls = ('/input', 'index')
@@ -21,6 +24,27 @@ token = os.environ.get("TELEGRAM_INMETBOT_APIKEY")
 # TELEGRAM CONFIG
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
+bot = telegram.Bot(token)
+# for update in bot.get_updates():
+    # print(update)
+
+
+# Decorators to simulate user feedback
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(update, context,  *args, **kwargs)
+        return command_func
+
+    return decorator
+
+send_typing_action = send_action(telegram.ChatAction.TYPING)
+send_upload_photo_action = send_action(telegram.ChatAction.UPLOAD_PHOTO)
+send_upload_video_action = send_action(telegram.ChatAction.UPLOAD_VIDEO)
 
 
 # BOT MESSAGES
@@ -32,28 +56,66 @@ Não filiado ao INMET
 Criado por @AtilioA
 """
 
-
 # BOT FUNCTIONS AND HANDLERS
+@send_typing_action
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=welcomeMessage, parse_mode="markdown")
 
-
+@send_upload_photo_action
 def vpr(update, context):
     vprImageURL = scrap_satelites.vpr_last_image()
     # print(vprImageURL)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=vprImageURL)
 
+@send_typing_action
+def alertas(update, context):
+    print("Getting alerts...")
+
+    alerts = parse_alerts.parse_alerts()
+    alertMessage = ""
+    for alert in alerts:
+        if alert.severity == "Perigo":
+            severityEmoji = "🔶"
+        else:
+            severityEmoji = "🚨"
+
+        area = ','.join(alert.area)
+
+        formattedStartDate = alert.startDate.strftime("%d/%m/%Y %H:%M")
+        formattedEndDate = alert.endDate.strftime("%d/%m/%Y %H:%M")
+
+        alertMessage += f"""
+{severityEmoji} *{alert.event}*
+
+        *Áreas afetadas*: {area}.
+        *Vigor*: De {formattedStartDate} a {formattedEndDate}.
+        {alert.description}
+
+"""
+        # Gráfico do alerta: {alert.graphURL}
+
+    alertMessage += "Veja os gráficos em http://www.inmet.gov.br/portal/alert-as/"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=alertMessage, parse_mode="markdown", disable_web_page_preview=True)
+
+@send_upload_video_action
+def sorrizoronaldo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="É O *SORRIZO RONALDO* 😁 QUE CHEGOU...", parse_mode="markdown")
+    sorrizoVideo = context.bot.get_file("BAACAgEAAxkBAAPmXkSUcBDsVM300QABV4Oerb9PcUx3AAL8AAODXihGe5y1jndyb80YBA")
+    context.bot.send_video(chat_id=update.effective_chat.id, video="BAACAgEAAxkBAAPmXkSUcBDsVM300QABV4Oerb9PcUx3AAL8AAODXihGe5y1jndyb80YBA")
+
 
 start_handler = CommandHandler('start', start)
 vpr_handler = CommandHandler('vpr', vpr)
+alertas_handler = CommandHandler('alertas', alertas)
+sorrizoronaldo_handler = CommandHandler('sorrizoronaldo', sorrizoronaldo)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(vpr_handler)
+dispatcher.add_handler(alertas_handler)
+dispatcher.add_handler(sorrizoronaldo_handler)
 
-# POOL
 updater.start_polling()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
-    print("Bot running")
---
+++ parse_alerts.py
@@ -0,0 +1,105 @@
+import re
+import os
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+class Alert():
+    def __init__(self, item=None):
+        if item:
+            self.get_event(item)
+            self.get_severity(item)
+            self.get_startDate(item)
+            self.get_endDate(item)
+            self.get_description(item)
+            self.get_area(item)
+            self.get_graphURL(item)
+        else:
+            self.event = ""
+            self.severity = ""
+            self.startDate = ""
+            self.endDate = ""
+            self.description = ""
+            self.area = ""
+            self.graphURL = ""
+
+    def get_event(self, item):
+        eventPattern = r"<title>(.*?)(?= Severidade)"  # "event" is same as "title"
+        eventMatch = re.search(eventPattern, str(item))
+        if eventMatch:
+            self.event = eventMatch.group(1)
+
+    def get_severity(self, item):
+        severityPattern = r"(?<=Severidade Grau: )(.*?)(?=</title)"
+        severityMatch = re.search(severityPattern, str(item))
+        if severityMatch:
+            self.severity = severityMatch.group(1)
+
+    def get_description(self, item):
+        descriptionPattern = r"Descrição</th><td>INMET publica aviso iniciando em: .*?\. (.*?)(?=</td>)"
+        descriptionMatch = re.search(descriptionPattern, str(item))
+        if descriptionMatch:
+            self.description = descriptionMatch.group(1)
+
+    def get_area(self, item):
+        areaPattern = r"(?<=Aviso para as áreas: )(.*?)(?=</td>)"
+        areaMatch = re.search(areaPattern, str(item))
+        if areaMatch:
+            self.area = areaMatch.group(1).split(",")
+
+    def get_graphURL(self, item):
+        graphURLPattern = r"Link Gráfico</th><td><a href=\"(.*?)\""
+        graphURLMatch = re.search(graphURLPattern, str(item))
+        if graphURLMatch:
+            self.graphURL = graphURLMatch.group(1)
+
+    def get_startDate(self, item):
+        startDatePattern = r"Início<\/th><td>(\d{4}-\d{2}-\d{2} \d\d:\d\d:\d\d\.\d)"
+        startDateMatch = re.search(startDatePattern, str(item))
+        if startDateMatch:
+            self.startDate = datetime.strptime(startDateMatch.group(1), '%Y-%m-%d %H:%M:%S.%f')
+
+    def get_endDate(self, item):
+        endDatePattern = r"Fim<\/th><td>(\d{4}-\d{2}-\d{2} \d\d:\d\d:\d\d\.\d)"
+        endDateMatch = re.search(endDatePattern, str(item))
+        if endDateMatch:
+            self.endDate = datetime.strptime(endDateMatch.group(1), '%Y-%m-%d %H:%M:%S.%f')
+
+def get_alerts_objects(items):
+    alertsObjects = []
+    dateNow = datetime.now()
+    for item in items:
+        alert = Alert(item)
+        if (alert.endDate > dateNow) and (alert.severity != "Perigo Potencial"):
+            alertsObjects.append(alert)
+
+    return alertsObjects
+
+
+def parse_alerts():
+    """ Extract Danger/Great Danger alerts from INMET's RSS feed """
+
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+
+    alertsURL = "https://alerts.inmet.gov.br/cap_12/rss/alert-as.rss"
+    req = requests.get(alertsURL, headers=headers, allow_redirects=False)
+    if req.status_code == 200:
+        print("Successful GET request to alerts RSS!")
+
+        # Retrieve the RSS feed content
+        content = req.content
+        xml = BeautifulSoup(content, 'html.parser')
+
+        # Get alerts from each item entry
+        items = xml.channel.find_all("item")
+        alerts = get_alerts_objects(items)
+
+        # print(len(alerts))
+        return alerts
+    else:
+        print("Failed GET request to alerts RSS.")
+
+
+if __name__ == "__main__":
+    parse_alerts()
