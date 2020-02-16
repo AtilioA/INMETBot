1a2a1460a9e293464a4c83995d59dfe5b0e0c4f4
AtilioA <atiliodadalto@hotmail.com>
2020-02-16 12:56:00 -0300

    Update webserver

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # Webserver to prevent the bot from sleeping
 import webserver
-import web
 
 from bot_config import updater
-from bot_functions import *
-from bot_handlers import *
+import bot_handlers
 
 updater.start_polling()
 
 if __name__ == "__main__":
-    app = web.application(webserver.urls, globals())
+    app = webserver.web.application(webserver.urls, globals())
     app.run()
--- webserver.py
+++ webserver.py
@@ -1,4 +1,8 @@
+import web
+
 urls = ('/', 'index')
 class index:
     def GET(self):
-        return "200"
+        i = web.input(name=None)
+        return render.index(i.name)
+render = web.template.render('templates/')
