aac70135f659ed266aebba350f5c44ef7ecb52db
AtilioA <atiliodadalto@hotmail.com>
2020-02-16 12:56:57 -0300

    Update bot messages

--- bot_messages.py
+++ bot_messages.py
@@ -6,10 +6,10 @@ welcomeMessage = """Olá! Este bot pode enviar imagens e informações úteis di
 `/vpr` ou `/nuvens` exibe imagens de vapor de água realçado (vapor de água na média e alta atmosfera)
 `/alertas` ou `/alertas_brasil` exibe alertas graves em vigor no Brasil
 `/alertas_CEP` exibe alertas graves em vigor para o CEP fornecido.
-Ex: `/alertas_CEP 29075-910`
-Alternativamente, basta enviar sua localização para checar alertas em sua cidade.
+Exemplo: `/alertas_CEP 29075-910`
+Você também pode enviar sua localização para checar alertas em sua cidade.
 `/acumulada` exibe imagem de precipitação acumulada no intervalo de dias especificado (1, 3, 5, 10, 15, 30 ou 90) anteriores ao atual no Brasil.
-Ex: `/acumulada 3`
+Exemplo: `/acumulada 3`
 `/acumulada_previsao_24hrs` exibe imagem de precipitação acumulada prevista para as próximas 24 horas no Brasil
 
 
@@ -18,18 +18,27 @@ Não filiado ao INMET
 Criado por @AtilioA
 """
 
-helpMessage = """Para utilizar o bot, envie alguns destes comandos:
+helpMessage = """🌥 @INMETBot
+Para utilizar o bot, envie alguns destes comandos:
 
+🕹 *COMANDOS DISPONÍVEIS*
 `/start` exibe a mensagem de boas-vindas
 `/help` ou `/ajuda` exibe esta mensagem de ajuda
 `/vpr` ou `/nuvens` exibe imagens de vapor de água realçado (vapor de água na média e alta atmosfera)
 `/alertas` ou `/alertas_brasil` exibe alertas graves em vigor no Brasil
 `/alertas_CEP` exibe alertas graves em vigor para o CEP fornecido.
-Ex: `/alertas_CEP 29075-910`
-Alternativamente, basta enviar sua localização para checar alertas em sua cidade.
+Exemplo: `/alertas_CEP 29075-910`
+Você também pode enviar sua localização para checar alertas em sua cidade.
 `/acumulada` exibe imagem de precipitação acumulada no intervalo de dias especificado (1, 3, 5, 10, 15, 30 ou 90) anteriores ao atual no Brasil.
-Ex: `/acumulada 3`
+Exemplo: `/acumulada 3`
 `/acumulada_previsao_24hrs` exibe imagem de precipitação acumulada prevista para as próximas 24 horas no Brasil
+
+📖 Bot open-source:
+https://github.com/AtilioA/INMETBot
+
+—
+Não filiado ao INMET
+Criado por @AtilioA
 """
 
 sorrizoChegou = "É O *SORRIZO RONALDO* 😁 QUE CHEGOU..."
