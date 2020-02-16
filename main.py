8a7f7e8db44c5968f15e284313b61cf30a3f453c
AtilioA <atiliodadalto@hotmail.com>
2020-02-16 00:09:40 -0300

    Solve warnings, remove "catch_all" from groups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Webserver to prevent the bot from sleeping
 import webserver
+import web
 
 from bot_config import updater
 from bot_functions import *
@@ -8,5 +9,5 @@ from bot_handlers import *
 updater.start_polling()
 
 if __name__ == "__main__":
-    app = webserver.web.application(webserver.urls, globals())
+    app = web.application(webserver.urls, globals())
     app.run()
