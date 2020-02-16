3dda9b9f1356d77be830a3fe9284fe06c8bda5f9
AtilioA <atiliodadalto@hotmail.com>
2020-02-16 13:03:22 -0300

    Document main functions, fix alertas_CEP, update handlers

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,23 +1,26 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler
 
 from bot_config import dispatcher, bot
-from bot_functions import *
+import bot_functions
 
 # Initialize handlers
-start_handler = CommandHandler('start', start)
-help_handler = CommandHandler(('help', 'ajuda'), help_msg)
+start_handler = CommandHandler('start', bot_functions.cmd_start)
+help_handler = CommandHandler(('help', 'ajuda'), bot_functions.cmd_help)
 
-vpr_handler = CommandHandler(('vpr', 'vapor_de_agua', 'nuvens'), vpr)
-vpr_gif_handler = CommandHandler('vpr_gif', vpr_gif)
+vpr_handler = CommandHandler(('vpr', 'vapor_de_agua', 'nuvens'), bot_functions.cmd_vpr)
+vpr_gif_handler = CommandHandler('vpr_gif', bot_functions.cmd_vpr_gif)
 
-acumulada_handler = CommandHandler('acumulada', acumulada)
-acumulada_previsao_24hrs_handler = CommandHandler('acumulada_previsao_24hrs', acumulada_previsao_24hrs)
+acumulada_handler = CommandHandler('acumulada', bot_functions.cmd_acumulada)
+acumulada_previsao_24hrs_handler = CommandHandler('acumulada_previsao_24hrs', bot_functions.cmd_acumulada_previsao_24hrs)
 
-alertas_brasil_handler = CommandHandler(('alertas', 'alertas_brasil', 'avisos'), alertas_brasil)
-alertas_CEP_handler = CommandHandler('alertas_CEP', alertas_CEP)
+alertas_brasil_handler = CommandHandler(('alertas', 'alertas_brasil', 'avisos'), bot_functions.cmd_alertas_brasil)
+alertas_CEP_handler = CommandHandler('alertas_CEP', bot_functions.cmd_alertas_CEP)
+alertas_location_handler = MessageHandler(Filters.location, bot_functions.alertas_location)
 
-sorrizoronaldo_handler = CommandHandler(('sorrizo', 'sorrizoronaldo', 'fodase'), sorrizoronaldo)
-sorrizoronaldo_will_rock_you_handler = CommandHandler(('sorrizoronaldo_will_rock_you', 'sorrizorock', 'sorrizoqueen', 'queenfodase'), sorrizoronaldo_will_rock_you)
+sorrizoronaldo_handler = CommandHandler(('sorrizo', 'sorrizoronaldo', 'fodase'), bot_functions.cmd_sorrizoronaldo)
+sorrizoronaldo_will_rock_you_handler = CommandHandler(('sorrizoronaldo_will_rock_you', 'sorrizorock', 'sorrizoqueen', 'queenfodase'), bot_functions.cmd_sorrizoronaldo_will_rock_you)
+
+catch_all_if_not_group_handler = MessageHandler(Filters.text, bot_functions.catch_all_if_not_group)
 
 # Add handlers to dispatcher
 dispatcher.add_handler(start_handler)
@@ -31,10 +34,9 @@ dispatcher.add_handler(acumulada_previsao_24hrs_handler)
 
 dispatcher.add_handler(alertas_brasil_handler)
 dispatcher.add_handler(alertas_CEP_handler)
-dispatcher.add_handler(MessageHandler(Filters.location, alertas_location))
 
 dispatcher.add_handler(sorrizoronaldo_handler)
 dispatcher.add_handler(sorrizoronaldo_will_rock_you_handler)
-dispatcher.add_handler(sorrizoronaldo_will_rock_you_handler)
 
-dispatcher.add_handler(MessageHandler(Filters.text, catch_all_if_not_group))
+dispatcher.add_handler(alertas_location_handler)
+dispatcher.add_handler(catch_all_if_not_group_handler)
--- bot_utils.py
+++ bot_utils.py
@@ -21,9 +21,41 @@ send_upload_document_action = send_action(telegram.ChatAction.UPLOAD_DOCUMENT)
 
 
 def determine_severity_emoji(severity):
+    """ Determine emoji for alert message. """
+
     if severity == "Perigo Potencial":
-        return "⚠️"
+        return "⚠️"  # Yellow alert
     elif severity == "Perigo":
-        return "🔶"
+        return "🔶"  # Orange alert
     else:
-        return "🚨"
+        return "🚨"  # Red alert
+
+
+def get_alert_message(alert, location=None):
+    severityEmoji = determine_severity_emoji(alert.severity)
+    area = ','.join(alert.area)
+    formattedStartDate = alert.startDate.strftime("%d/%m/%Y %H:%M")
+    formattedEndDate = alert.endDate.strftime("%d/%m/%Y %H:%M")
+
+    if location:
+        header = f"{severityEmoji} *{alert.event[:-1]} para {location}.*"
+    else:
+        header = f"{severityEmoji} *{alert.event}*"
+
+    messageString = f"""
+{header}
+
+        *Áreas afetadas*: {area}.
+        *Vigor*: De {formattedStartDate} a {formattedEndDate}.
+        {alert.description}
+"""
+    return messageString
+
+
+def is_group_or_channel(chat_id):
+    """ Check if current chat is group or channel. """
+
+    if update.effective_chat.id < 0:
+        return True
+    else:
+        return False
