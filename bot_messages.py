92b016f7065a4dfe77319cb26ed0185dbffea99c
AtilioA <atiliodadalto@hotmail.com>
2020-02-15 23:33:15 -0300

    Modularize bot and show alerts through location

---
+++ bot_messages.py
@@ -0,0 +1,36 @@
+welcomeMessage = """Olá! Este bot pode enviar imagens e informações úteis disponíveis no site do INMET diretamente pelo Telegram.
+
+*COMANDOS DISPONÍVEIS*:
+`/start` exibe esta mensagem de boas-vindas
+`/help` ou `/ajuda` exibe a mensagem de ajuda
+`/vpr` ou `/nuvens` exibe imagens de vapor de água realçado (vapor de água na média e alta atmosfera)
+`/alertas` ou `/alertas_brasil` exibe alertas graves em vigor no Brasil
+`/alertas_CEP` exibe alertas graves em vigor para o CEP fornecido.
+Ex: `/alertas_CEP 29075-910`
+Alternativamente, basta enviar sua localização para checar alertas em sua cidade.
+`/acumulada` exibe imagem de precipitação acumulada no intervalo de dias especificado (1, 3, 5, 10, 15, 30 ou 90) anteriores ao atual no Brasil.
+Ex: `/acumulada 3`
+`/acumulada_previsao_24hrs` exibe imagem de precipitação acumulada prevista para as próximas 24 horas no Brasil
+
+
+—
+Não filiado ao INMET
+Criado por @AtilioA
+"""
+
+helpMessage = """Para utilizar o bot, envie alguns destes comandos:
+
+`/start` exibe a mensagem de boas-vindas
+`/help` ou `/ajuda` exibe esta mensagem de ajuda
+`/vpr` ou `/nuvens` exibe imagens de vapor de água realçado (vapor de água na média e alta atmosfera)
+`/alertas` ou `/alertas_brasil` exibe alertas graves em vigor no Brasil
+`/alertas_CEP` exibe alertas graves em vigor para o CEP fornecido.
+Ex: `/alertas_CEP 29075-910`
+Alternativamente, basta enviar sua localização para checar alertas em sua cidade.
+`/acumulada` exibe imagem de precipitação acumulada no intervalo de dias especificado (1, 3, 5, 10, 15, 30 ou 90) anteriores ao atual no Brasil.
+Ex: `/acumulada 3`
+`/acumulada_previsao_24hrs` exibe imagem de precipitação acumulada prevista para as próximas 24 horas no Brasil
+"""
+
+sorrizoChegou = "É O *SORRIZO RONALDO* 😁 QUE CHEGOU..."
+sorrizoQueen = "👊👊👏 *SORRIZ*.."
