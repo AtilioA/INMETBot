144e6c498499c176d1609f52d83235a542a37149
AtilioA <atiliodadalto@hotmail.com>
2020-02-20 17:33:01 -0300

    Fix webdriver for Heroku

--- parse_alerts.py
+++ parse_alerts.py
@@ -21,10 +21,11 @@ def take_screenshot_alerts_map():
     chrome_options.add_argument("--window-size=540x1080")
 
     if 'ON_HEROKU' in os.environ:
-        GOOGLE_CHROME_PATH = os.environ.get('GOOGLE_CHROME_PATH')
-        CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH')
-        chrome_options.binary_location = GOOGLE_CHROME_PATH
-        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     else:
         chrome_driver = os.path.join(os.getcwd(), "chromedriver.exe")
         driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
