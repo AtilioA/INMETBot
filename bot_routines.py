97057316c2f715809126c78f96fe463cceff53c4
AtilioA <atiliodadalto@hotmail.com>
2020-02-23 09:26:10 -0300

    Change notify_chats_routine logic

--- bot_routines.py
+++ bot_routines.py
@@ -11,7 +11,7 @@ routinesLogger.setLevel(logging.DEBUG)
 
 
 def delete_past_alerts_routine():
-    """ Delete past alerts published by INMET from the database. """
+    """Delete past alerts published by INMET from the database."""
 
     alerts = list(models.INMETBotDB.alertsCollection.find({}))
     timeNow = arrow.utcnow().to("Brazil/East")
@@ -23,10 +23,12 @@ def delete_past_alerts_routine():
 
 
 def parse_alerts_routine(ignoreModerate=False):
-    """ Parse alerts published by INMET and insert them into database.
+    """Parse alerts published by INMET and insert them into database.
 
-    Args:
-        ignoreModerate: if set to True, will ignore alerts of moderate severity. Defaults to False.
+    Parameters
+    --------
+    ignoreModerate : bool
+        If set to True, will ignore alerts of moderate severity. Defaults to False.
     """
 
     alertsXML = parse_alerts.parse_alerts_xml(ignoreModerate)
@@ -38,45 +40,47 @@ def parse_alerts_routine(ignoreModerate=False):
 
 
 def notify_chats_routine():
-    """ Check alerts from the database and notify chats.
+    """Check alerts from the database and notify chats.
 
     For every alert and chat, check if chat has been notified for that alert; if it has not, check if there is any city (obtained by CEPs in the chat's CEP list) that is included in the alert.
     If there is, notify chat and mark chat as notified (so it won't get notified again for the same alert).
     """
 
     subscribedChats = list(models.INMETBotDB.subscribedChatsCollection.find({}))
-    alerts = list(models.INMETBotDB.alertsCollection.find({}))
+    alertMessage = ""
 
     for chat in subscribedChats:
-        routinesLogger.debug(f"Checking chat {chat['chatID']}")
-        for alert in alerts:
-            if chat["chatID"] not in alert["notifiedChats"]:
-                warnedCities = []
-                for cep in chat["CEPs"]:
-                    try:
-                        city = viacep.get_cep_city(cep)
-                        routinesLogger.debug(f"Checking {city}...")
-                        if city in alert["cities"]:
-                            warnedCities.append(city)
-                            routinesLogger.info(f"-- Appended warned city. --")
-                    except Exception as error:
-                        routinesLogger.error(error)
+        routinesLogger.debug(f"- Checking chat {chat['chatID']}")
+        for cep in chat["CEPs"]:
+            try:
+                city = viacep.get_cep_city(cep)
+                routinesLogger.debug(f"Checking {city}...")
+            except Exception as error:
+                routinesLogger.warning(f"Viacep error: {error}")
+                continue
 
-                if warnedCities:
+            alerts = list(models.INMETBotDB.alertsCollection.find(
+                {"$and": [
+                    {"cities": city}, {"notifiedChats": {"$ne": chat["chatID"]}}
+                ]}
+            ))
+            if alerts:
+                routinesLogger.info(f"-- Warning about {city}. --")
+                for alert in alerts:
                     alertObj = models.Alert(alertDict=alert)
+                    alertMessage += alertObj.get_alert_message(city)
                     routinesLogger.info(f"-- Notifying chat {chat['chatID']} for alert {alert['alertID']}... --")
 
-                    alertMessage = alertObj.get_alert_message(warnedCities)
-                    alertMessage += "\nMais informações em http://www.inmet.gov.br/portal/alert-as/"
-
-                    updater.bot.send_message(chat_id=chat["chatID"], text=alertMessage, parse_mode="markdown", disable_web_page_preview=True)
                     models.INMETBotDB.alertsCollection.update_one({"alertID": alert["alertID"]}, {"$addToSet": {"notifiedChats": chat['chatID']}})
+
+                alertMessage += "\nMais informações em http://www.inmet.gov.br/portal/alert-as/"
+                updater.bot.send_message(chat_id=chat["chatID"], text=alertMessage, parse_mode="markdown", disable_web_page_preview=True)
 
     routinesLogger.info("Finished notify_chats_routine routine.")
 
 
 if __name__ == "__main__":
-    parse_alerts_routine()
-    # notify_chats_routine()
+    # parse_alerts_routine()
+    notify_chats_routine()
     # delete_past_alerts_routine()
     pass
