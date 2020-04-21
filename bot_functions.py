4cd49ec32af8e33224a6c32cd0ea71b4955e1c82
AtilioA <atiliodadalto@hotmail.com>
2020-04-21 20:18:43 -0300

    Handle Telegram-related exceptions on notify_chats_routine
    Cherry-picked from deploy branch

--- bot_functions.py
+++ bot_functions.py
@@ -381,5 +381,4 @@ def cmd_sorrizoronaldo_will_rock_you(update, context):
 @run_async
 def error(update, context):
     """Log errors caused by Updates."""
-
-    functionsLogger.warning('Update "%s" caused error "%s"', update, context.error)
+    functionsLogger.error('Update "%s" caused error "%s"', update, context.error)
