1907d5f3a1de4feedcf4ab28799e2a0074404503
AtilioA <atiliogpg@protonmail.com>
2022-02-13 19:20:16 -0300

    Try fixing routines

--- main.py
+++ main.py
@@ -26,28 +26,32 @@ logging.basicConfig(
 )
 
 
+try:
+    schedule.every(ROUTINES_INTERVAL).minutes.do(parse_alerts_routine)
+    schedule.every(ROUTINES_INTERVAL).minutes.do(delete_past_alerts_routine)
+    schedule.every(ROUTINES_INTERVAL).minutes.do(notify_chats_routine)
+
+    schedule.every(ROUTINES_INTERVAL).minutes.do(
+        ping, URL="https://inmetbot.herokuapp.com/"
+    )
+    schedule.every(ROUTINES_INTERVAL).minutes.do(
+        ping, URL="https://chooseipsum.herokuapp.com/"
+    )
+    for i in range(12, 24):
+        schedule.every().day.at(f"{i}:00").do(
+            ping, URL="https://covid19nowbot.herokuapp.com/"
+    )
+except:
+    logging.exception("Error in main routine.")
+    pass
+
+
 # Thread for running routines periodically
 class RoutinesThread(Thread):
     def run(self):
         while True:
             schedule.run_pending()
             time.sleep(60)
-
-
-schedule.every(ROUTINES_INTERVAL).minutes.do(parse_alerts_routine)
-schedule.every(ROUTINES_INTERVAL).minutes.do(delete_past_alerts_routine)
-schedule.every(ROUTINES_INTERVAL).minutes.do(notify_chats_routine)
-
-schedule.every(ROUTINES_INTERVAL).minutes.do(
-    ping, URL="https://inmetbot.herokuapp.com/"
-)
-schedule.every(ROUTINES_INTERVAL).minutes.do(
-    ping, URL="https://chooseipsum.herokuapp.com/"
-)
-for i in range(12, 24):
-    schedule.every().day.at(f"{i}:00").do(
-        ping, URL="https://covid19nowbot.herokuapp.com/"
-    )
 
 
 def main():
